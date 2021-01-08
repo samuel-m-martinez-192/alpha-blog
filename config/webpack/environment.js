ccd15fcd263cd06e598fdd14c7b15d554927de1d
Your name <youremail@example.com>
2021-01-08 12:11:47 -0700

    Adding Bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Pooper: ['popper.js', 'default']
+}))
+
 module.exports = environment
