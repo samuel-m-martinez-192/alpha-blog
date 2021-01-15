e79e9a576c8c3a4b3d26bcdf5220852e1b09781f
Samuel Martinez <samuel.m.martinez.192@gmail.com>
2021-01-15 11:20:49 -0700

    Added delete action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     #@user = User.find(params[:id])
@@ -38,6 +38,16 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render "new"
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      session[:user_id] = nil
+      flash[:notice] = "Account and all associated articles sucessfully deleted"
+      redirect_to root_path
+    else
+      flash[:notice] = "There was a problem deleting your account"
     end
   end
 
