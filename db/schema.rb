b414f269617c2d2735d0969f7aceb02f9f31d4a7
Samuel Martinez <samuel.m.martinez.192@gmail.com>
2021-01-18 01:02:48 -0700

    Category model with unit testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_192137) do
+ActiveRecord::Schema.define(version: 2021_01_18_075230) do
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_01_15_192137) do
     t.string "password_digest"
     t.boolean "admin", default: false
   end
+
 end
