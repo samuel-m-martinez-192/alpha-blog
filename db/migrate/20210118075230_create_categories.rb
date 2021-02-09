f4ebf462cecfbae3f7c8382fa5df184fa3342865
Samuel Martinez <samuel.m.martinez.192@gmail.com>
2021-02-09 11:51:33 -0700

    Validations using unit test

--- db/migrate/20210118075230_create_categories.rb
+++ db/migrate/20210118075230_create_categories.rb
@@ -1,4 +1,6 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
+  #To create a new migration table use the console
+  # rails generate migration create_categories
   def change
     create_table :categories do |t|
       t.string :name
